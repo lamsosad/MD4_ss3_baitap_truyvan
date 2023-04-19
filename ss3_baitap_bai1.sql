c7257079c71a1ca450e1589c73f36af9bf94c7ad
lamsosad <119550328+lamsosad@users.noreply.github.com>
2023-04-19 15:17:22 +0700

    Add files via upload

---
+++ ss3_baitap_bai1.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE QuanLySinhVien;
+USE QuanLySinhVien;
+CREATE TABLE Class
+(
+    ClassID   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ClassName VARCHAR(60) NOT NULL,
+    StartDate DATETIME    NOT NULL default now(),
+    Status    BIT
+);
+CREATE TABLE Student
+(
+    StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StudentName VARCHAR(30) NOT NULL,
+    Address     VARCHAR(50),
+    Phone       VARCHAR(20),
+    Status      BIT,
+    ClassId     INT         NOT NULL,
+    FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
+);
+CREATE TABLE Subject
+(
+    SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubName VARCHAR(30) NOT NULL,
+    Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
+    Status  BIT                  DEFAULT 1
+);
+CREATE TABLE Mark
+(
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
+
+insert into Class(ClassName,StartDate,Status) values
+("A1","2008-12-20",1),
+("A2","2008-12-22",1),
+("B3",now(),0);
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId) VALUES 
+('Hung', 'Ha Noi', '0912113113', 1, 1),
+('Hoa', 'Hai phong',null, 1, 1),
+('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO Subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+       
+INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1),
+       (1, 3, 2, 1);
+
+select * from Student where student.StudentName like "h%";
+select * from Class where month(class.StartDate) = 12;
+select * from Subject where subject.Credit between 3 and 5;
+select * from Subject where subject.Credit between 3 and 5;
+
+select s.StudentName,sub.SubName,m.Mark from 
+(student s join mark m on s.StudentId=m.StudentId)
+join subject sub on sub.SubId=m.SubId
+
+order by m.Mark asc, m.Mark desc;
+
+
+
+
+
+
